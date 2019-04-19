481649735a1900d40b6f11d91af0bef734cee3e3
unrealisher <unrealmpa3@mail.ru>
2019-04-19 14:21:19 +0300

    Настройка хероку

---
+++ index.js
@@ -0,0 +1,21 @@
+/*eslint-disable*/
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+/*eslint-enable*/
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.static(path.resolve("build")));
+app.get("/", (req, res) => res.sendFile(path.resolve("build/index.html")));
+
+app.use("*", (req, res) => {
+  return res.status(404).send("<h1>Page not found</h1>");
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
